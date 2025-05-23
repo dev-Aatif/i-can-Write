bdaedae6f69c25f18fda72be0d042dc72af4d472
dev-Aatif <Aatiff.P@proton.me>
2025-05-23 15:16:05 +0500

    Update blogLoader.js

--- js/blogLoader.js
+++ js/blogLoader.js
@@ -7,6 +7,8 @@ async function loadBlogPosts() {
         // Fetch the list of blog posts from your blogs directory
         const response = await fetch('/blogs/posts.json');
         const posts = await response.json();
+        const posts = data.posts;
+
 
         posts.forEach(post => {
             const article = createBlogPostElement(post);
@@ -22,12 +24,13 @@ function createBlogPostElement(post) {
     article.className = 'blog-post';
     
     article.innerHTML = `
-        <img src="${post.image || 'https://via.placeholder.com/400x200'}" alt="${post.title}">
+        <img src="${post.image}" alt="${post.title}">
         <div class="post-content">
             <h2>${post.title}</h2>
             <div class="post-meta">
-                <span><i class="far fa-calendar"></i> ${post.date}</span>
+                <span><i class="far fa-calendar"></i> ${new Date(post.date).toLocaleDateString()}</span>
                 <span><i class="far fa-folder"></i> ${post.tags[0]}</span>
+                <span><i class="far fa-user"></i> ${post.author}</span>
             </div>
             <p>${post.description}</p>
             <a href="/blogs/${post.path}" class="read-more">Read More</a>
@@ -35,6 +38,15 @@ function createBlogPostElement(post) {
     `;
     
     return article;
+}
+
+function showErrorMessage() {
+    const blogContainer = document.querySelector('.blog-grid');
+    blogContainer.innerHTML = `
+        <div class="error-message">
+            <p>Sorry, we couldn't load the blog posts at this time. Please try again later.</p>
+        </div>
+    `;
 }
 
 // Load posts when the page loads
